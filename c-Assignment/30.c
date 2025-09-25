3293e37729314c0445d29f24f0db397181dbb7d1
sahil <sahil.v.chaudhari.cg@gmail.com>
2025-09-25 11:15:13 +0530

    C Assignments Push

--- c-Assignment/30.c
+++ c-Assignment/30.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int row;
-    scanf("%d",&row);
+    int n;
+    scanf("%d",&n);
 
-    for (int i = 1; i <= row; i++){
-        for(int j = 1; j <= row - i; j++){
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n - i; j++){
             printf(" ");
         }
-        for(int k = 1; k <=2 * i - 1; k++){
+        for (int k = 1; k <= i; k++){
+            printf("%c",'A'+k-1);
+        }
+        for (int k = i-1; k >= 1; k--){
             printf("%c",'A'+k-1);
         }
         printf("\n");
     }
 
-    for (int i = row - 1; i >= 1; --i){
-       for(int j = 1; j <= row - i; j++){
-        printf(" ");
-       }
-       for(int k = 1; k <= 2 * i - 1; k++){
-        printf("%c",'A'+k-1);
-       }
-       printf("\n");
+    for (int i = n - 1; i >= 1; i--){
+        for (int j = 1; j <= n - i; j++){
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++){
+            printf("%c",'A'+k-1);
+        }
+        for (int k = i-1; k >= 1; k--){
+            printf("%c",'A'+k-1);
+        }
+        printf("\n");
     }
-    
-    
+
     return 0;
 }
